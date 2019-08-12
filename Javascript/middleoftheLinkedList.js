7eaad769eb20baa43521d2af185f8b66a3d3c8a1
dev1203 <devsingh120397@gmail.com>
2019-08-11 21:32:11 -0700

    Added solutions of  Middle of the Linked List

---
+++ Javascript/middleoftheLinkedList.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    if (head.next === null) {
+        return head;
+    }
+    
+    let length = 0;
+    let current = head;
+    
+    while ( current != null) {
+        length++;
+        current = current.next;
+    }
+    
+    length = Math.floor(length/2);
+    current = head;
+    for (let index = 0; index< length; index ++) {
+            current = current.next
+    }
+    return current;
+};
